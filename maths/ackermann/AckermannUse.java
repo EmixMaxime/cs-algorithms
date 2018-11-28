68a29bbe4a990e94cbf68445dbd2d2515b501456
Unknown <emixmaxime@outlook.fr>
2018-11-28 21:19:55 +0100

    (fix) ackermann use check m,n natural numbers check

--- maths/ackermann/AckermannUse.java
+++ maths/ackermann/AckermannUse.java
@@ -4,6 +4,10 @@ class AckermannUse
     {
         int m = 3, n = 4;
         int result = Ackermann.ackermann(m, n);
+
+        if (m < 0 || n < 0) {
+            throw new ArithmeticException("m and n have to be natural numbers.");
+        }
     
         // Should be 125
         System.out.printf("akermann(%d,%d) = %d \n", m, n, result);
